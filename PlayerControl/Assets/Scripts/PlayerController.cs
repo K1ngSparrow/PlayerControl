7b7f0ac74af7b7fa57177b5079138eb22372c9f5
John <royj@roverkids.org>
2024-04-16 13:39:00 -0400

    1.2 complete

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Move move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
+
 }
